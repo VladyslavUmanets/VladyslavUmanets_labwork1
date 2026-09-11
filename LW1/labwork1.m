8e8e1c11aafe967f48920e6f55bd9901a890b0ee
VladyslavUmanets <ovoch228822@gmail.com>
2026-09-11 22:36:44 +0300

    Update labwork1.m

--- LW1/labwork1.m
+++ LW1/labwork1.m
@@ -1,7 +1,8 @@
 % Name: Vladyslav Umanets
-% Group:EDIfu25/1
+% Group: EDIfu25/1
 % Date: 11.09.2026
 % Laboratory Work 1
+
 %% Mandatory Task 6 points
 x = 1:32;
 y = x.^2;
@@ -10,18 +11,19 @@ plot(x, y, 'o-r', x, y/3, 'xb')
 title('Two Functions')
 xlabel('X axis')
 ylabel('F_1 [-o-]    F_2 [-x-]')
+
 %% Complementary Task (4 points), var-8
 N = 8;
 
 V = (N+1):0.5:(N+4);
 
-A = [N N+1 N+2; N+3 N+4 N+5 ;N+6 N+7 N+8];
+A = [N,   N+1, N+2; 
+     N+3, N+4, N+5; 
+     N+6, N+7, N+8];
 
 sub_a = A(3, 2);
-
 sub_b = A(2:3, 1:2);
-
 sub_c = A([1, 3], [1, 3]);
-s
+
 V_col = V(1:3)';
 A_concatenated = [A, V_col];
